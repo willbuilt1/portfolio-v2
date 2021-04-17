c0059dfc26468deaff694412f31d024b9fc5aeb8
Will <wfoster12@gmail.com>
2021-04-17 11:51:56 +0100

    env check

--- pages/index.js
+++ pages/index.js
@@ -6,21 +6,23 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Willbuilt.co.uk</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Will Foster - Web Developer</h1>
+        <h1 className={styles.title}>
+          Will Foster<br></br>Web Developer
+        </h1>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
         </a>
       </footer>
     </div>
