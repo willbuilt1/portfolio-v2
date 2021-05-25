37ab301b48cc940dec0c2b1a065a2cfd8554e735
Will <wfoster12@gmail.com>
2021-05-25 14:18:39 +0100

    SlideIn animation tidy

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState } from 'react';
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
+  const [showLinks, setshowLinks] = useState(false);
+
+  const handleHover = () => console.log('hoverring');
+  // const handleHover = () => setshowLinks(!showLinks);
   return (
     <div className='container'>
       <Head>
@@ -13,28 +18,28 @@ export default function Home() {
       <main className='main'>
         <Link href='/about'>
           <a className={styles.link}>
-            <span className={styles.link__initial}>Hi.</span>
-            <span className={styles.link__location}>About</span>
+            <div className={styles.link__container}>
+              <span className={styles.link__initial} onMouseEnter={handleHover}>
+                Hi.
+              </span>
+              <span className={styles.link__location}>About</span>
+            </div>
           </a>
         </Link>
         <Link href='/about'>
           <a className={styles.link}>
-            <span
-              className={`${styles.link__initial} ${styles.link__initialMid}`}
-            >
-              I am
-            </span>
-            <span className={styles.link__location}>Work</span>
+            <div className={`${styles.link__container} ${styles.delay02}`}>
+              <span className={styles.link__initial}>I am</span>
+              <span className={styles.link__location}>Work</span>
+            </div>
           </a>
         </Link>
         <Link href='/about'>
           <a className={styles.link}>
-            <span
-              className={`${styles.link__initial} ${styles.link__initialBottom}`}
-            >
-              Will
-            </span>
-            <span className={styles.link__location}>Contact</span>
+            <div className={`${styles.link__container} ${styles.delay03}`}>
+              <span className={`${styles.link__initial} `}>Will</span>
+              <span className={styles.link__location}>Contact</span>
+            </div>
           </a>
         </Link>
       </main>
