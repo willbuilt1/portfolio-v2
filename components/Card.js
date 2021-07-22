d050e213faffe2cc1d6faec411bdded39c74450a
willbuilt1 <wfoster12@gmail.com>
2021-07-22 14:54:01 +0100

    Added Contentful CMS backend

--- components/Card.js
+++ components/Card.js
@@ -1,30 +1,33 @@
 import Image from 'next/image';
 import styles from '../styles/components/Card.module.scss';
 import { useRouter } from 'next/router';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-const Card = ({ work }) => {
+const Card = ({ portfolio }) => {
+  console.log(portfolio);
   const router = useRouter();
 
   return (
     <div
       className={styles.card}
-      onClick={() => router.push(`/work/${work.slug}`)}
+      onClick={() => router.push(`/work/${portfolio.slug}`)}
     >
       <div className={styles.card__top}>
         <Image
-          src={work.image}
-          layout='responsive'
-          height='1080'
-          width='1920'
-          objectFit='cover'
+          src={portfolio.thumbnail.url}
+          layout="responsive"
+          height="1080"
+          width="1920"
+          objectFit="cover"
           className={styles.card__image}
         ></Image>
-        <h2 className={styles.card__topText}>{work.title}</h2>
+        <h2 className={styles.card__topText}>{portfolio.siteName}</h2>
       </div>
-      <div
+      {/* <div
         className={styles.card__body}
         dangerouslySetInnerHTML={work.description}
-      ></div>
+      ></div> */}
+      <div>{documentToReactComponents(portfolio.desc.json)}</div>
     </div>
   );
 };
--- pages/work.js
+++ pages/work.js
@@ -1,28 +1,57 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { GraphQLClient, gql } from 'graphql-request';
 import Layout, { siteTitle } from '../components/layout';
 import work from '../data/workData';
 import styles from '../styles/components/Work.module.scss';
 import Card from '../components/Card';
 
 export async function getStaticProps() {
+  const endpoint = `https://graphql.contentful.com/content/v1/spaces/${process.env.SPACE_ID}`;
+
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${process.env.AUTH_TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    {
+      portfolioCollection {
+        items {
+          siteName
+          desc {
+            json
+          }
+          slug
+          thumbnail {
+            url
+          }
+        }
+      }
+    }
+  `;
+
+  const data = await graphQLClient.request(query);
+  // console.log(JSON.stringify(data, undefined, 2));
   return {
     props: {
       work,
+      data: data.portfolioCollection.items,
     },
   };
 }
-export default function Work({ work }) {
+export default function Work({ work, data }) {
   return (
     <Layout>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <h1 className='title'>Work</h1>
+      <h1 className="title">Work</h1>
       <section>
         <div className={styles.container}>
-          {work.map((item, index) => (
-            <Card key={index} work={item} />
+          {data.map((item, index) => (
+            <Card key={index} portfolio={item} />
           ))}
         </div>
       </section>
